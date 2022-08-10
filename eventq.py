048536b29150349505536bfa6adc4b4aad5d13a2
Robert Painter <robertpainter@MacBook-Pro.local>
2022-08-10 08:03:46 +0100

    started on eventQ (missed some files)

--- eventq.py
+++ eventq.py
@@ -10,9 +10,18 @@ class Event_Message:
 class EventQ:
     def __init__(self):
         self.event_q = []
+        self.subcribers = []
         self.latest_id = 0
 
     def append(self, message):
         message["id"] = self.latest_id + 1
         self.event_q.append(message)
+        # self.notify_subscribers()
+
+    def register_subscriber(self, subscriber):
+        self.subscriber.append(subscriber)
+
+    def notify_subscribers(self)
+        for s in subscriber:
+            s.update(message)
 
--- hpl.py
+++ hpl.py
@@ -1,7 +1,7 @@
 from dataflow import *
 from eventq import *
 
-event_q = EventQ()
+eventq = EventQ()
 
 meg_hpl = App('meg_hpl')
 meg_hpl.api_call("frtb_in_scope_map_POST", "frtb_in_scope_map")
